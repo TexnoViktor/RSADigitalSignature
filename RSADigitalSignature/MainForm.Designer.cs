8d0701ae953c35578c6141c265ea134f64f0632c
TexnoViktor <ushakovviktor14@gmail.com>
2024-11-20 00:04:13 +0100

    Добавьте файлы проекта.

---
+++ RSADigitalSignature/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RSADigitalSignature
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.LoadPrivateKey = new System.Windows.Forms.Button();
+            this.LoadPublicKey = new System.Windows.Forms.Button();
+            this.GenerateKeys = new System.Windows.Forms.Button();
+            this.SignMessage = new System.Windows.Forms.Button();
+            this.VerifySignature = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(189, 78);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(415, 20);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // LoadPrivateKey
+            // 
+            this.LoadPrivateKey.Location = new System.Drawing.Point(211, 175);
+            this.LoadPrivateKey.Name = "LoadPrivateKey";
+            this.LoadPrivateKey.Size = new System.Drawing.Size(144, 23);
+            this.LoadPrivateKey.TabIndex = 1;
+            this.LoadPrivateKey.Text = "Зчитати закритий ключ";
+            this.LoadPrivateKey.UseVisualStyleBackColor = true;
+            this.LoadPrivateKey.Click += new System.EventHandler(this.LoadPrivateKey_Click);
+            // 
+            // LoadPublicKey
+            // 
+            this.LoadPublicKey.Location = new System.Drawing.Point(466, 175);
+            this.LoadPublicKey.Name = "LoadPublicKey";
+            this.LoadPublicKey.Size = new System.Drawing.Size(138, 23);
+            this.LoadPublicKey.TabIndex = 2;
+            this.LoadPublicKey.Text = "Зчитати відкритий ключ";
+            this.LoadPublicKey.UseVisualStyleBackColor = true;
+            this.LoadPublicKey.Click += new System.EventHandler(this.LoadPublicKey_Click);
+            // 
+            // GenerateKeys
+            // 
+            this.GenerateKeys.Location = new System.Drawing.Point(360, 128);
+            this.GenerateKeys.Name = "GenerateKeys";
+            this.GenerateKeys.Size = new System.Drawing.Size(104, 23);
+            this.GenerateKeys.TabIndex = 3;
+            this.GenerateKeys.Text = "Генерація ключів";
+            this.GenerateKeys.UseVisualStyleBackColor = true;
+            this.GenerateKeys.Click += new System.EventHandler(this.GenerateKeys_Click);
+            // 
+            // SignMessage
+            // 
+            this.SignMessage.Location = new System.Drawing.Point(211, 250);
+            this.SignMessage.Name = "SignMessage";
+            this.SignMessage.Size = new System.Drawing.Size(144, 23);
+            this.SignMessage.TabIndex = 4;
+            this.SignMessage.Text = "Підписати повідомлення";
+            this.SignMessage.UseVisualStyleBackColor = true;
+            this.SignMessage.Click += new System.EventHandler(this.SignMessage_Click);
+            // 
+            // VerifySignature
+            // 
+            this.VerifySignature.Location = new System.Drawing.Point(466, 250);
+            this.VerifySignature.Name = "VerifySignature";
+            this.VerifySignature.Size = new System.Drawing.Size(138, 23);
+            this.VerifySignature.TabIndex = 5;
+            this.VerifySignature.Text = "Перевірити підпис";
+            this.VerifySignature.UseVisualStyleBackColor = true;
+            this.VerifySignature.Click += new System.EventHandler(this.VerifySignature_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VerifySignature);
+            this.Controls.Add(this.SignMessage);
+            this.Controls.Add(this.GenerateKeys);
+            this.Controls.Add(this.LoadPublicKey);
+            this.Controls.Add(this.LoadPrivateKey);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button LoadPublicKey;
+        private System.Windows.Forms.Button GenerateKeys;
+        private System.Windows.Forms.Button SignMessage;
+        private System.Windows.Forms.Button VerifySignature;
+        private System.Windows.Forms.Button LoadPrivateKey;
+    }
+}
+
---
+++ RSADigitalSignature/MainForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSADigitalSignature
+{
+    public partial class MainForm : Form
+    {
+        private string privateKeyPath = string.Empty;
+        private string publicKeyPath = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Генерація ключів
+        private void GenerateKeys_Click(object sender, EventArgs e)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                // Зберігаємо закритий ключ у файл
+                File.WriteAllText("privateKey.xml", rsa.ToXmlString(true));
+                // Зберігаємо відкритий ключ у файл
+                File.WriteAllText("publicKey.xml", rsa.ToXmlString(false));
+                MessageBox.Show("Ключі згенеровані та збережені!");
+            }
+        }
+
+        // Зчитування закритого ключа
+        private void LoadPrivateKey_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "XML Files (*.xml)|*.xml",
+                Title = "Виберіть файл із закритим ключем"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                privateKeyPath = openFileDialog.FileName;
+                MessageBox.Show($"Закритий ключ завантажено: {privateKeyPath}");
+            }
+        }
+
+        // Зчитування відкритого ключа
+        private void LoadPublicKey_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "XML Files (*.xml)|*.xml",
+                Title = "Виберіть файл із відкритим ключем"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                publicKeyPath = openFileDialog.FileName;
+                MessageBox.Show($"Відкритий ключ завантажено: {publicKeyPath}");
+            }
+        }
+
+        // Підписання повідомлення
+        private void SignMessage_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(privateKeyPath))
+            {
+                MessageBox.Show("Будь ласка, завантажте закритий ключ.");
+                return;
+            }
+
+            string message = txtMessage.Text;
+
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                MessageBox.Show("Будь ласка, введіть повідомлення для підпису.");
+                return;
+            }
+
+            string privateKey = File.ReadAllText(privateKeyPath);
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.FromXmlString(privateKey);
+
+                // Перетворюємо повідомлення у байти
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                // Генеруємо хеш повідомлення
+                using (SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider())
+                {
+                    byte[] hash = sha1.ComputeHash(messageBytes);
+
+                    // Створюємо цифровий підпис
+                    byte[] signature = rsa.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
+
+                    // Зберігаємо підпис у файл
+                    File.WriteAllBytes("signature.sig", signature);
+                    MessageBox.Show("Повідомлення підписане та підпис збережено у файл signature.sig.");
+                }
+            }
+        }
+
+        // Перевірка підпису
+        private void VerifySignature_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(publicKeyPath))
+            {
+                MessageBox.Show("Будь ласка, завантажте відкритий ключ.");
+                return;
+            }
+
+            string message = txtMessage.Text;
+
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                MessageBox.Show("Будь ласка, введіть повідомлення для перевірки.");
+                return;
+            }
+
+            // Вибираємо файл підпису
+            OpenFileDialog openSignatureDialog = new OpenFileDialog
+            {
+                Filter = "Signature Files (*.sig)|*.sig",
+                Title = "Виберіть файл із підписом"
+            };
+
+            if (openSignatureDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            byte[] signature = File.ReadAllBytes(openSignatureDialog.FileName);
+
+            string publicKey = File.ReadAllText(publicKeyPath);
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.FromXmlString(publicKey);
+
+                // Перетворюємо повідомлення у байти
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                // Генеруємо хеш повідомлення
+                using (SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider())
+                {
+                    byte[] hash = sha1.ComputeHash(messageBytes);
+
+                    // Перевіряємо підпис
+                    bool isValid = rsa.VerifyHash(hash, CryptoConfig.MapNameToOID("SHA1"), signature);
+
+                    MessageBox.Show(isValid ? "Підпис дійсний!" : "Підпис недійсний!");
+                }
+            }
+        }
+    }
+}
